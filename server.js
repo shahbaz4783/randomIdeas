9cc9e97c2efdbd2394f521f2f3c61ee15726d2c4
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-16 21:22:48 +0530

    create update delete

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const ideasRouter = require('./routes/ideas')
 
 const app = express();
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res) => {
     res.json({message: 'Welcome to Random Ideas API'});
