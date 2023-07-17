b5edf3987d7b736488fe1fb18bf050683313eca0
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-17 12:52:33 +0530

    crud rest api fully functional

--- routes/ideas.js
+++ routes/ideas.js
@@ -2,31 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Idea = require('../models/Idea')
 
-// Ideas Object
-const ideas = [
-	{
-		id: 1,
-		text: 'This is First Idea',
-		tag: 'Technology',
-		username: 'mike23',
-		date: '13-02-22',
-	},
-	{
-		id: 2,
-		text: 'This is Second Idea',
-		tag: 'Invention',
-		username: 'alex12',
-		date: '13-02-22',
-	},
-	{
-		id: 3,
-		text: 'This is Third Idea',
-		tag: 'Software',
-		username: 'steve31',
-		date: '21-06-22',
-	},
-];
-
 // Get all ideas
 router.get('/', async (req, res) => {
     try {
@@ -39,16 +14,14 @@ router.get('/', async (req, res) => {
 });
 
 // Get particular idea
-router.get('/:id', (req, res) => {
-	const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-	if (!idea) {
-		return res
-			.status(404)
-			.json({ success: false, error: 'Requested query not available' });
-	}
-
-	res.json({ success: true, data: idea });
+router.get('/:id', async (req, res) => {
+	try {
+        const idea = await Idea.findById(req.params.id);
+        res.json({ success: true, data: idea })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, data: 'Something went wrong' })
+    }
 });
 
 
@@ -71,36 +44,35 @@ router.post('/', async (req, res) => {
 })
 
 // Update ideas
-router.put('/:id', (req, res) => {
-	const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-	if (!idea) {
-		return res
-			.status(404)
-			.json({ success: false, error: 'Requested query not available' });
-	}
-
-    idea.text = req.body.text || idea.text;
-    idea.tag = req.body.tag || idea.tag;
-
-	res.json({ success: true, data: idea });
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedIdea = await Idea.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    text: req.body.text,
+                    tag: req.body.tag
+                }
+            },
+            { new: true }
+        )
+        res.json({ success: true, data: updatedIdea })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, data: 'Something went wrong' })
+    }
 });
 
 
 // Delete ideas
-router.delete('/:id', (req, res) => {
-	const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-	if (!idea) {
-		return res
-			.status(404)
-			.json({ success: false, error: 'Requested query not available' });
-	}
-
-    const index = ideas.indexOf(idea);
-    ideas.splice(index, 1)
-
-	res.json({ success: true, data: {} });
+router.delete('/:id', async (req, res) => {
+	try {
+        const deleteIdea = await Idea.findByIdAndDelete(req.params.id)
+        res.json({ success: true, data: deleteIdea })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, data: 'Something went wrong' })
+    }
 });
 
 
