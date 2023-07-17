1535c7feca1ea4dff91fcde110905b14741382e0
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-17 21:18:21 +0530

    modal component for hide show form

---
+++ client/src/components/Modal.js
@@ -0,0 +1,30 @@
+class Modal {
+    constructor() {
+        this._modal = document.getElementById('modal');
+        this._modalBtn = document.getElementById('modal-btn');
+        this.eventListeners();
+    }
+
+    eventListeners() {
+        this._modalBtn.addEventListener('click', this.open.bind(this));
+        window.addEventListener('click', this.outsideClick.bind(this));
+    }
+
+    open() {
+        this._modal.style.display = 'block';
+    }
+
+    close() {
+        this._modal.style.display = 'none';
+    }
+
+    outsideClick(e) {
+        if (e.target === this._modal){
+            this.close();
+        };
+
+    }
+}
+
+
+export default Modal;
